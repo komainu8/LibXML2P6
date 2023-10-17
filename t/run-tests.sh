da932d0b7f6aee124fb5b6d8437459fd69f2e302
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-10-17 17:01:15 +0900

    test: fix a typo

--- t/run-tests.sh
+++ t/run-tests.sh
@@ -2,7 +2,6 @@
 
 set -xue
 
-/host/rakudo/bin/raku --version
 export PATH=$PATH:/host/rakudo/bin:/host/rakudo/share/perl6/site/bin:/host/rakudo/share/perl6/vendor/bin:/host/rakudo/share/perl6/core/bin
 echo "Run tests!"
 prove6 --lib /host/t/
