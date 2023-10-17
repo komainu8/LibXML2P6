887cec7f148388c5edcb8cea6d806618320048f1
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-10-17 20:40:38 +0900

    test: move location

--- run-tests.sh
+++ run-tests.sh
@@ -3,6 +3,8 @@
 set -xue
 
 export PATH=$PATH:/host/rakudo/bin:/host/rakudo/share/perl6/site/bin:/host/rakudo/share/perl6/vendor/bin:/host/rakudo/share/perl6/core/bin
+echo "Echo Raku lang version"
+raku --version
 echo "Run tests!"
 prove6 --lib /host/t/
 echo "Finished tests!"
